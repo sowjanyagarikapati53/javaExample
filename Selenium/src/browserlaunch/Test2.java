7fa7e6a665069185abd327b80f34e3815b4a5089
sowjanyagarikapati53 <sowjanyagarikapati53@gmail.com>
2022-05-30 11:06:34 +0530

    addind abc variables

--- Selenium/src/browserlaunch/Test2.java
+++ Selenium/src/browserlaunch/Test2.java
@@ -2,8 +2,8 @@ package browserlaunch;
 
 public class Test2
 {
-
-	public static void main(String[] args)
+	int a= 10,b=20,c=30;
+public static void main(String[] args)
 	{
 		System.out.println("git project");
 		System.out.println("git project 1");
